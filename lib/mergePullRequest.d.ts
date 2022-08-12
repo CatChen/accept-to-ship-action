29e546c01670eaab4a7258a76373075221b8f309
Cat Chen <catchen@catchen.me>
2022-08-12 14:00:07 -0400

    Checking if PR is merged before Action execution

--- lib/mergePullRequest.d.ts
+++ lib/mergePullRequest.d.ts
@@ -1,3 +1,4 @@
 import { Octokit } from "@octokit/core";
 import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
+export declare function checkIfPullRequestMerged(owner: string, repo: string, pullRequestNumber: number, octokit: Octokit & Api): Promise<boolean>;
 export declare function mergePullRequest(owner: string, repo: string, pullRequestNumber: number, octokit: Octokit & Api): Promise<void>;
--- src/mergePullRequest.ts
+++ src/mergePullRequest.ts
@@ -1,6 +1,29 @@
 import { Octokit } from "@octokit/core";
 import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
 
+export async function checkIfPullRequestMerged(
+  owner: string,
+  repo: string,
+  pullRequestNumber: number,
+  octokit: Octokit & Api
+) {
+  const response = await octokit.rest.pulls.checkIfMerged({
+    owner,
+    repo,
+    pull_number: pullRequestNumber,
+  });
+
+  if (response.status === 204) {
+    return true;
+  } else if (response.status === 404) {
+    return false;
+  } else {
+    throw new Error(
+      `Failed to check if pull request is merged: ${response.status}`
+    );
+  }
+}
+
 export async function mergePullRequest(
   owner: string,
   repo: string,
@@ -13,6 +36,6 @@ export async function mergePullRequest(
     pull_number: pullRequestNumber,
   });
   if (response.status !== 200) {
-    throw new Error(`Failed to merge pull request: ${pullRequestNumber}`);
+    throw new Error(`Failed to merge pull request: ${response.status}`);
   }
 }
