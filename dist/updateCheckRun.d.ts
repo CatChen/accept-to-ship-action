58d7c90e361871d570a8573f9194aac79c3a3974
Cat Chen <catchen@catchen.me>
2022-10-23 21:06:38 -0700

    Removed unused module (#1206)

--- dist/updateCheckRun.d.ts
+++
@@ -1,4 +0,0 @@
-import { Octokit } from "@octokit/core";
-import { Api } from "@octokit/plugin-rest-endpoint-methods/dist-types/types";
-import { components } from "@octokit/openapi-types/types";
-export declare function updateCheckRun(owner: string, repo: string, runId: number, conclusion: components["schemas"]["check-run"]["conclusion"], octokit: Octokit & Api): Promise<void>;
