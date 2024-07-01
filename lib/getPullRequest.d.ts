a27560861644c0f2bc029c03eff87e54b511263b
Cat Chen <catchen@catchen.me>
2024-07-01 07:36:15 +0000

    Renamed dist to bundle; renamed lib to dist and ignored it (#3268)

--- lib/getPullRequest.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Octokit } from '@octokit/core';
-import type { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types';
-import type { PullRequest } from '@octokit/webhooks-definitions/schema';
-export declare function getPullRequest(owner: string, repo: string, pullRequestNumber: number, octokit: Octokit & Api): Promise<PullRequest>;
