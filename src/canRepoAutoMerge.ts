c0e6687e541e7a42545315dc1a1d856002859378
Cat Chen <catchen@catchen.me>
2026-03-04 00:17:23 -0800

    Set up TypeScript and ESLint for GraphQL (#13109)

--- src/canRepoAutoMerge.ts
+++ src/canRepoAutoMerge.ts
@@ -1,27 +1,32 @@
+import type { ResultOf, VariablesOf } from '@graphql-typed-document-node/core';
 import type { Octokit } from '@octokit/core';
 import type { Api } from '@octokit/plugin-rest-endpoint-methods';
+import { graphql } from './__graphql__/gql.js';
+
+const queryCanRepoAutoMerge = graphql(`
+  query CanRepoAutoMerge($owner: String!, $repo: String!) {
+    repository(owner: $owner, name: $repo) {
+      id
+      autoMergeAllowed
+    }
+  }
+`);
 
 export async function canRepoAutoMerge(
   owner: string,
   repo: string,
   octokit: Octokit & Api,
 ): Promise<boolean> {
-  const {
-    repository: { autoMergeAllowed },
-  } = await octokit.graphql<{
-    repository: { autoMergeAllowed: boolean };
-  }>(
-    `
-      query($owner: String!, $repo: String!) {
-        repository(owner: $owner, name: $repo) {
-          autoMergeAllowed
-        }
-      }
-    `,
-    {
-      owner,
-      repo,
-    },
-  );
-  return autoMergeAllowed;
+  const { repository } = await octokit.graphql<
+    ResultOf<typeof queryCanRepoAutoMerge>
+  >(queryCanRepoAutoMerge.toString(), {
+    owner,
+    repo,
+  } satisfies VariablesOf<typeof queryCanRepoAutoMerge>);
+  if (repository == null) {
+    throw new Error(
+      `Failed to check if the repo is auto-mergeable through GraphQL: ${owner}/${repo}`,
+    );
+  }
+  return repository.autoMergeAllowed;
 }
